4d454900f7b7a04baee5c0293ab85f8ac3c02a0f
rpaternina <robertpaternina65@gmail.com>
2024-10-22 15:51:02 -0500

    GET, POST, PUT, PATCH listo

--- src/main/java/Estudiantes/Controllers/StudentController.java
+++ src/main/java/Estudiantes/Controllers/StudentController.java
@@ -20,22 +20,24 @@ public class StudentController {
 
     /**
      * Metodo para obtener todos los estudiantes
+     *
      * @return
      */
     @GetMapping
-    public List<Student> getStudents(){
+    public List<Student> getStudents() {
         return estudiantes;
     }
 
     /**
      * Metodo para buscar un estudiante por email
+     *
      * @param email
      * @return
      */
     @GetMapping("/{email}")
-    public Student getStuden(@PathVariable String email){
-        for(Student StudendByEmail : estudiantes){
-            if (StudendByEmail.getEmail().equals(email)){
+    public Student getStuden(@PathVariable String email) {
+        for (Student StudendByEmail : estudiantes) {
+            if (StudendByEmail.getEmail().equals(email)) {
                 return StudendByEmail;
             }
         }
@@ -44,19 +46,21 @@ public class StudentController {
 
     /**
      * Metodo para agregar un estudiante
+     *
      * @param estudiante
      * @return
      */
     @PostMapping
-    public Student postStuden(@RequestBody Student estudiante){
-    estudiantes.add(estudiante);
-    return estudiante;
+    public Student postStuden(@RequestBody Student estudiante) {
+        estudiantes.add(estudiante);
+        return estudiante;
     }
 
+
     @PutMapping
-    public Student putStuden(@RequestBody Student estudiante){
-        for(Student updateStudent : estudiantes){
-            if(updateStudent.getId() == estudiante.getId()){
+    public Student putStuden(@RequestBody Student estudiante) {
+        for (Student updateStudent : estudiantes) {
+            if (updateStudent.getId() == estudiante.getId()) {
                 updateStudent.setName(estudiante.getName());
                 updateStudent.setEmail(estudiante.getEmail());
                 updateStudent.setAge(estudiante.getAge());
@@ -67,5 +71,29 @@ public class StudentController {
         return null;
     }
 
+    /**
+     * Metodo para editar algun campo en espeficio de un estudiante
+     * @param estudiante
+     * @return
+     */
+    @PatchMapping
+    public Student patchStuden(@RequestBody Student estudiante) {
+        for(Student editarEspecifico : estudiantes){
+            if(editarEspecifico.getId() == estudiante.getId()){
+                if(estudiante.getName() != null){
+                    editarEspecifico.setName(estudiante.getName());
+                }if(editarEspecifico.getEmail() != null){
+                    editarEspecifico.setEmail(estudiante.getEmail());
+                }//if(editarEspecifico.getAge() != null){
+                   // editarEspecifico.setAge(estudiante.getAge());
+                if(editarEspecifico.getCourse() != null){
+                    editarEspecifico.setCourse(estudiante.getCourse());
+                }
+                return editarEspecifico;
+            }
+        }
+        return null;
+    }
+    
 
 }
