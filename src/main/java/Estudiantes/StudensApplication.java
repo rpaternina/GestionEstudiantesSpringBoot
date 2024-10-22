9c930ee17336f1a39446f4eeee9cedea0231632a
rpaternina <robertpaternina65@gmail.com>
2024-10-22 15:18:45 -0500

    Listo GET y POST

---
+++ src/main/java/Estudiantes/StudensApplication.java
@@ -0,0 +1,13 @@
+package Estudiantes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudensApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudensApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Estudiantes/StudensApplicationTests.java
@@ -0,0 +1,13 @@
+package Estudiantes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudensApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
